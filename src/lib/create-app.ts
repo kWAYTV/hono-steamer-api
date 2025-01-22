c8f94359b999905f768d4d742bb54f750270bf24
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-01-22 18:56:25 +0100

    refactor: remove unused favicon middleware import from create-app.ts

--- src/lib/create-app.ts
+++ src/lib/create-app.ts
@@ -1,6 +1,6 @@
 import { serveStatic } from "@hono/node-server/serve-static";
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { notFound, onError, serveEmojiFavicon } from "stoker/middlewares";
+import { notFound, onError } from "stoker/middlewares";
 import { defaultHook } from "stoker/openapi";
 
 import { pinoLogger } from "@/middlewares/pino-logger";
