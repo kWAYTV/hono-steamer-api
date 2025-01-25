dcaa815718e1135830411184ce114c36bb574c0f
kWAY <55437151+kWAYTV@users.noreply.github.com>
2025-01-25 10:01:11 +0100

    refactor: remove unused import from index route

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,7 +1,6 @@
 import { createRoute, z } from "@hono/zod-openapi";
 import * as HttpStatusCodes from "stoker/http-status-codes";
 import { jsonContent } from "stoker/openapi/helpers";
-import { createMessageObjectSchema } from "stoker/openapi/schemas";
 
 import { ApiSuccessResponseSchema } from "@/helpers/api-response.schemas";
 import { createRouter } from "@/lib/create-app";
